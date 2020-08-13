8afad7a78ecf828b68e4e00561707a333766b735
steverodrigz <hi@stevenarodriguez.com>
2020-08-12 22:42:48 -0400

    update form

---
+++ assets/2020/js/formspree.js
@@ -0,0 +1,4 @@
+<script>
+  window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+  formbutton("create", {action: "https://formspree.io/mqkyjypy"})
+</script>
