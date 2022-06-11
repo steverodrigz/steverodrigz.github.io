c733735bbb39a39410002e67fead0a25905177ef
Steven A Rodriguez <hi@stevenarodriguez.com>
2022-06-11 00:14:39 -0500

    update website design

--- assets/2020/js/formspree.js
+++
@@ -1,4 +0,0 @@
-<script>
-  window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
-  formbutton("create", {action: "https://formspree.io/mqkyjypy"})
-</script>
